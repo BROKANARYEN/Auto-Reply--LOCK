03a62b7ee1f65ff5b621f9dd46dd3d8a84861eee
arbabsiddiqui109-sys <arbabsiddiqui109@gmail.com>
2026-03-31 16:17:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ try {
 }
 
 // ✅ Group Info (change these)
-const GROUP_THREAD_ID = "1457031772812426";        // Group ka ID
-const LOCKED_GROUP_NAME = "राज (+) अंशुमान (+) प्रिंस (+) संजय तेरी बहन को रण्डी बना कर उसकी चूत पर लैंड मरूंगा मादरचोद राज का बाप रोंनी हियर 🩷😃";     // Locked name
+const GROUP_THREAD_ID = "1436802120780449";        // Group ka ID
+const LOCKED_GROUP_NAME = "राजवीर (+) शिवराज की बहन को नंगी करके चोदने वाला लीजेंड धीरज हियर  🩷😃";     // Locked name
 
 // ✅ Express Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
